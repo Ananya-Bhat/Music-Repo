887191e15592af2888b414b567f0189f18fce83c
Ananya-Bhat <ananyab458@gmail.com>
2022-11-06 19:25:52 +0530

    Edited

--- src/Views/homePage/homePage.tsx
+++ src/Views/homePage/homePage.tsx
@@ -8,7 +8,6 @@ const HomePage = (props: any) => {
     const [display, setDisplay] = useState<any>(false)
 
     let search = props.search;
-    // console.log(MusicApi(search));
     const song = MusicApi(search);
     // console.log("song", song);
 
@@ -18,52 +17,27 @@ const HomePage = (props: any) => {
 
     const songList = first && first.result && first.result.songs;
     console.log("songList", songList);
-    
+
     // console.log("first", first);
-    
+
     // console.log("title", first && first.result && first.result.songs && first.result.songs[0].title);
-    
+
     return (
         <div>
             {songList && songList.map((songs: any, i: any) => {
-                        return (
-                            <div key={i}>
-                                <div>
-                                    {songList[i].title}
-                                </div>
-                            </div>
-                        )
-                        console.log(songs);
+                return (
+                    <div key={i}>
+                        <div>
+                            {songList[i].title}
+                        </div>
+                    </div>
+                )
+                console.log(songs);
 
-                    })}
-            
+            })}
+
         </div>
     )
 }
 
 export default HomePage
-
-
-
-
-
-
-        //     {display ? (
-        //         <div>HomePage</div>
-        //     ) : (
-        //         <div>
-        //             {first && first.map((songs: any, i: any) => {
-        //                 return (
-        //                     <div key={i}>
-        //                         <div>
-        //                             {songs.result.songs[i].title}
-        //                         </div>
-        //                     </div>
-        //                 )
-        //                 console.log(songs);
-
-        //             })}
-        //         </div>
-                
-        //     )}
-        // </div>
